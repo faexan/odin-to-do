6756299cba341c6eba8700f3ea613329f274baf2
Faizan <faizani4680@gmail.com>
2024-09-20 01:07:49 +0500

    added animations to the sidebar

--- src/menuDiv.js
+++ src/menuDiv.js
@@ -1,15 +1,15 @@
 
 
-const menuDisplay = function() {
+const menuDisplay = function () {
 
 
 
-    const  menuIcon = document.querySelector(".menuIcon");
-    const menuDiv = document.querySelector(".menuDiv"); 
+    const menuIcon = document.querySelector(".menuIcon");
+    const menuDiv = document.querySelector(".menuDiv");
     const mainScreenDiv = document.querySelector(".mainScreenDiv");
 
 
-    menuIcon.addEventListener("click", ()=> {
+    menuIcon.addEventListener("click", () => {
         menuDiv.classList.toggle("sidebarActive");
         mainScreenDiv.classList.toggle("mainDivActive");
     })
@@ -17,13 +17,14 @@ const menuDisplay = function() {
 
 
 const aPMheading = document.querySelector(".APMheading");
+const addProjectForm = document.querySelector(".addProjectForm");
+const addProjectCancel = document.querySelector(".APFbtn2");
 
-
-aPMheading.addEventListener("click", ()=> {
-
-    
-
-
+aPMheading.addEventListener("click", () => {
+    addProjectForm.classList.toggle("formExpanded");
+})
+addProjectCancel.addEventListener("click", ()=> {
+    addProjectForm.classList.toggle("formExpanded");
 })
 
 
@@ -31,6 +32,5 @@ aPMheading.addEventListener("click", ()=> {
 
 
 
-
-export {menuDisplay};
+export { menuDisplay };
 
