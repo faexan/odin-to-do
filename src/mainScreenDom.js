439d143d6bf39e0a9d5134f34068220e0c66e391
Faizan <faizani4680@gmail.com>
2024-09-22 02:09:31 +0500

    fixed the mainScreen dom stuff

--- src/mainScreenDom.js
+++ src/mainScreenDom.js
@@ -5,44 +5,99 @@ const mainScreenDom = function () {
     const alltasksDiv = document.querySelector(".allTasksDiv");
     const todayTasksDiv = document.querySelector(".todayTasksDiv");
     const sevenTasksDiv = document.querySelector(".sevenDaysTasks");
-    const impTasks = document.querySelector(".imptTasks");
+    const impTasks = document.querySelector(".impTasks");
+    const projects = document.querySelectorAll(".PMLitems");
 
     const allTasksBtn = document.querySelector("#allTasks")
     const todayTasksBtn = document.querySelector("#todayTasks")
     const sevenTasksBtn = document.querySelector("#sevendaysTasks")
     const impTasksBtn = document.querySelector("#imptTasks")
 
+
+    const mainScreeHeading = document.querySelector(".mainScreenheading");
+
+
+
+
+
     const sidebarBtns = document.querySelectorAll(".HMLitems");
     sidebarBtns.forEach((btn)=> {
         btn.addEventListener("click", () =>  {
             if (btn.id == "allTasks") {
-                alltasksDiv.style.display = "inline";
-                todayTasksDiv.style.display = "none";
-                sevenTasksDiv.style.display = "none";
-                impTasks.style.display = "none";
-                  
+                mainScreeHeading.innerHTML = "All Tasks"                  
             } else if (btn.id == "todayTasks") {
-                todayTasksDiv.style.display = "inline";
-                alltasksDiv.style.display = "none";
-                sevenTasksDiv.style.display = "none";
-                impTasks.style.display = "none";
+                mainScreeHeading.innerHTML = "Today Tasks"
             } else if (btn.id == "sevenDaysTasks") {
-                sevenTasksDiv.style.display = "inline";
-                alltasksDiv.style.display = "none";
-                todayTasksBtn.style.display = "none";
-                impTasks.style.display = "none";
+                mainScreeHeading.innerHTML = "Next 7 days Tasks"
             } else if (btn.id == "impTasks") {
-                impTasks.style.display = "inline";
-                alltasksDiv.style.display = "none";
-                todayTasksDiv.style.display = "none";
-                sevenTasksDiv.style.display = "none";
-
+                mainScreeHeading.innerHTML = "Important Tasks"
+            } else if (btn.id == "defaultProject") {
+                mainScreeHeading.innerHTML = "Default Project"
             }
         })
     })
 
 
+
+
 }
+
+
+
+
+
+const createTaskOnDom = function() {
+    const tasksDiv = document.querySelector(".tasksDiv");
+
+
+    const mainDiv = document.createElement("div");
+    const div = document.createElement("div");
+    const div2 = document.createElement("div");
+    const checkBox = document.createElement("input");
+    const taskName = document.createElement("span");
+    const taskDetails = document.createElement("button");
+
+
+    checkBox.type = "checkbox";
+    taskDetails.type = "button";
+    taskDetails.innerHTML = "Details";
+    taskName.innerHTML = "Some Stuff I have to do";
+
+
+
+    mainDiv.classList.add("tasks");
+    checkBox.classList.add("TaskCheckBox");
+    taskName.classList.add("taskName");
+    taskDetails.classList.add("taskDetailsBtn");
+
+
+
+
+
+
+
+
+    div.appendChild(checkBox);
+    div.appendChild(taskName);
+    div2.appendChild(taskDetails);
+
+
+    mainDiv.appendChild(div);
+    mainDiv.appendChild(div2);
+
+
+
+    tasksDiv.appendChild(mainDiv);
+
+
+    
+}
+
+
+
+
+
+createTaskOnDom();
 
 
 export default mainScreenDom;
